30254425c2401ce7d88481a1896ee0cff99a8038
joblackpoc <duopoc@gmail.com>
2021-12-08 00:28:29 +0700

    First Commit

---
+++ DjangoAPI/EmployeeApp/serializers.py
@@ -0,0 +1,13 @@
+from django.db.models import fields
+from rest_framework import serializers
+from EmployeeApp.models import Departments,Employees
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Departments
+        fields=('DepartmentID','DepartmentName')
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model:Employees
+        fields=('EmployeeID','EmployeeName','Department','DateOfJoining','PhotoFileName')
